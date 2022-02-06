1e047e840af0d112168ec528a7cf35c602030fa0
Alina Prokofeva <whitestring@inbox.ru>
2022-02-07 00:01:29 +0300

    Changed val commission

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -1,9 +1,12 @@
 package ru.netology
+
+import kotlin.math.roundToInt
+
 fun main() {
-    val amount = 504000
+    val amount = 504100
     val minCommission = 3500
     val percent = 0.0075
-    val commission = (percent*amount).toInt()
+    val commission = (percent*amount).roundToInt()
     val result = if (commission <= minCommission) minCommission else commission
     val rouble = result/100
     val penny = result % 100
